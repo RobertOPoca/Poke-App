ee86993f1ee4fe0da8458b4cb4e1e7d60426e066
Roberto Pocasangre <pocasangre870@gmail.com>
2022-10-22 09:39:46 -0600

    :tada: Init App

---
+++ Poke-App/Poke-App/SupportingFiles/Poke_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poke_AppApp.swift
+//  Poke-App
+//
+//  Created by Roberto De la O Pocasangre on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Poke_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
